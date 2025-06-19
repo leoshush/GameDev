7deda3604d88f1f59c3f655159bf402d9bc8e10b
Ashish <singhashish33466@gmail.com>
2025-06-19 08:27:48 +0530

    Game Scene

--- sonicBoom/src/scenes/mainMenu.js
+++ sonicBoom/src/scenes/mainMenu.js
@@ -1,5 +1,5 @@
-
 import k from "../kaplayCtx"
+import { makeSonic } from "../entities/sonic";
 
 export default function mainMenu() {
     if(!k.getData("best-score")) k.setData("best-score",0);
@@ -9,22 +9,31 @@ export default function mainMenu() {
      k.add([k.sprite("chemical-bg"),k.pos(0,0),k.scale(2),k.opacity(0.8)])
        , k.add([k.sprite("chemical-bg"), k.pos(1920, 0), k.scale(2), k.opacity(0.8)])
     ];
-    
     const platformWidth = 1280;
-
-
     const platforms = [
         k.add([k.sprite("platforms"),k.pos(0,450),k.scale(4)]),
         k.add([k.sprite("platforms"),k.pos(384,450),k.scale(4)]),
     ];
+
+    k.add([k.text("SONIC RING RUN",{font:"mania",size:96}),k.pos(k.center().x,100),k.anchor("center")])
+    k.add([k.text("Press Space/Click/Touch to Play",{font:"mania",size:32}),k.pos(k.center().x,k.center().y-200),k.anchor("center")])
+    
+    makeSonic(k.vec2(200,745))
     k.onUpdate(()=>{
         if(bgPieces[1].pos.x<0){
             bgPieces[0].moveTo(bgPieces[1].pos.x +bgPieceWidth*2,0);
             bgPieces.push(bgPieces.shift());
 
         }
-        bgPieces[0].move(-3000,0)
+        bgPieces[0].move(-100,0)
         bgPieces[1].moveTo(bgPieces[0].pos.x+bgPieceWidth*2,0);
+       
+       if(platforms[1].pos.x<0){
+        platforms[0].moveTo(platforms[1].pos.x + platforms[1].width*4,450);
+        platforms.push(platforms.shift());
+       } 
+       platforms[0].move(-4000,0);
+       platforms[1].moveTo(platforms[0].pos.x + platforms[1].width*4,450);
 
     });
 }
