4dcaca1c63eea1a7d3f71d150220967568ec582a
Ashish <singhashish33466@gmail.com>
2025-06-19 22:29:23 +0530

    Done

--- sonicBoom/src/main.js
+++ sonicBoom/src/main.js
@@ -1,5 +1,6 @@
 import k from "./kaplayCtx"
 import game from "./scenes/game";
+import gameover from "./scenes/gameover";
 import mainMenu from "./scenes/mainMenu";
 
 k.loadSprite("chemical-bg","graphics/chemical-bg.png")
@@ -40,5 +41,5 @@ k.scene("main-menu",
    mainMenu
 )
 k.scene("game",game)
-k.scene("gameover",()=>{});
+k.scene("gameover",gameover);
 k.go("main-menu")
